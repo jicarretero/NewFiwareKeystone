67395118ea1e38f1ff4f7d01c191c9006fbfe147
José Ignacio Carretero Guarde <joseignacio.carretero@gmail.com>
2018-09-21 15:55:10 +0200

    User Change region

---
+++ user_change_region.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+user=$1
+[ -z $user ] && (echo "necesita como parametro el usuario" ; exit 1)
+region_id=$2
+[ -z $new_region ] && (echo "Necesita region como parametro" ; exit 1)
+
+project_id=$(openstack user show ${user} | awk 'BEGIN {done=0}
+     /cloud_project_id/ {id=$4 ; done=1}
+     /default_project_id/ {dpid=$4}
+     END {if (done==1) {print id} else {print dpid}}'
+)
+token=$(openstack token issue | awk '/ id / {print $4}')
+
+old_endpoint_group=$(curl ${OS_AUTH_URL}/OS-EP-FILTER/projects/${project_id}/endpoint_groups \
+     -H "X-AUTH-TOKEN: ${token}" -H "Content-Type: application/json" 2>/dev/null | jq -r .endpoint_groups[].id)
+
+endpoint_group=$(curl ${OS_AUTH_URL}/OS-EP-FILTER/endpoint_groups/ -H "X-AUTH-TOKEN: ${token}" -H "Content-Type: application/json" 2>/dev/null | jq -r ".endpoint_groups[] | select(.filters.region_id==\"${region_id}\") | .id")
+
+
+curl ${OS_AUTH_URL}/OS-EP-FILTER/endpoint_groups/${old_endpoint_group}/projects/${project_id}  -X DELETE -H "X-AUTH-TOKEN: ${token}" -H "Content-Type: application/json"
+curl ${OS_AUTH_URL}/OS-EP-FILTER/endpoint_groups/${endpoint_group}/projects/${project_id}  -X PUT -H "X-AUTH-TOKEN: ${token}" -H "Content-Type: application/json"
+
